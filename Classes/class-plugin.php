6377e7cb57f0d27eb210843ccbb656753a9814bf
Nico Martin <mail@nicomartin.ch>
2017-08-02 15:33:27 +0200

    added some php-notice eliminations

--- Classes/class-plugin.php
+++ Classes/class-plugin.php
@@ -6,17 +6,19 @@ class Plugin {
 
 	private static $instance;
 
-	public static $name = '';
-	public static $prefix = '';
-	public static $version = '';
-	public static $file = '';
+	public $name = '';
+	public $prefix = '';
+	public $version = '';
+	public $file = '';
 
 	public static $option_key = 'PLUGIN_PREFIX_data';
 
 	/**
 	 * Creates an instance if one isn't already available,
 	 * then return the current instance.
+	 *
 	 * @param  string $file The file from which the class is being instantiated.
+	 *
 	 * @return object       The class instance.
 	 */
 	public static function get_instance( $file ) {
@@ -25,21 +27,18 @@ class Plugin {
 
 			self::$instance = new Plugin;
 
-			if ( get_option( self::$option_key ) ) {
-
-				$data = get_option( self::$option_key );
-				self::$instance->name = $data['Name'];
-				self::$instance->version = $data['Version'];
-
+			if ( get_option( awpp_get_instance()->option_key ) ) {
+				$data = get_option( awpp_get_instance()->option_key );
 			} else {
-
-				self::$instance->name = self::$name;
-				self::$instance->version = '0.0.1';
-
+				$data = get_plugin_data( $file );
 			}
 
-			self::$instance->prefix = 'PLUGIN_PREFIX';
-			self::$instance->file = $file;
+			self::$instance->name    = $data['Name'];
+			self::$instance->version = $data['Version'];
+
+			self::$instance->prefix = 'awpp';
+			self::$instance->debug  = true;
+			self::$instance->file   = $file;
 
 			self::$instance->run();
 		}
@@ -48,32 +47,33 @@ class Plugin {
 	}
 
 	/**
-	* Non-essential dump function to debug variables.
-	* @param  mixed  $var The variable to be output
-	* @param  boolean $die Should the script stop immediately after outputting $var?
-	*/
+	 * Non-essential dump function to debug variables.
+	 *
+	 * @param  mixed $var The variable to be output
+	 * @param  boolean $die Should the script stop immediately after outputting $var?
+	 */
 	public function dump( $var, $die = false ) {
 		echo '<pre>' . print_r( $var, 1 ) . '</pre>';
 		if ( $die ) {
-	    	die();
+			die();
 		}
 	}
 
 	/**
-	* Execution function which is called after the class has been initialized.
-	* This contains hook and filter assignments, etc.
-	*/
+	 * Execution function which is called after the class has been initialized.
+	 * This contains hook and filter assignments, etc.
+	 */
 	private function run() {
 		add_action( 'plugins_loaded', array( $this, 'load_plugin_textdomain' ) );
 		add_action( 'admin_init', array( $this, 'update_plugin_data' ) );
-		register_deactivation_hook( self::$file, array( $this, 'deactivate' ) );
+		register_deactivation_hook( PLUGIN_PREFIX_get_instance()->file, array( $this, 'deactivate' ) );
 	}
 
 	/**
-	* Load translation files from the indicated directory.
-	*/
+	 * Load translation files from the indicated directory.
+	 */
 	public function load_plugin_textdomain() {
-		load_plugin_textdomain( 'TEXT_DOMAIN', false, dirname( plugin_basename( self::$file ) ) . '/languages' );
+		load_plugin_textdomain( 'TEXT_DOMAIN', false, dirname( plugin_basename( PLUGIN_PREFIX_get_instance()->file ) ) . '/languages' );
 	}
 
 	/**
@@ -81,21 +81,41 @@ class Plugin {
 	 */
 	public function update_plugin_data() {
 
-		$db_data = get_option( self::$option_key );
+		$db_data   = get_option( self::$option_key );
 		$file_data = get_plugin_data( self::$instance->file );
 
 		if ( ! $db_data || version_compare( $file_data['Version'], $db_data['Version'], '>' ) ) {
 
 			$new_option = array(
 				'Version' => $file_data['Version'],
-				'Name' => $file_data['Name'],
+				'Name'    => $file_data['Name'],
 			);
 
-			self::$instance->name = $new_option['Name'];
+			self::$instance->name    = $new_option['Name'];
 			self::$instance->version = $new_option['Version'];
 
 			update_option( self::$option_key, $new_option );
-			
+
+			if ( ! $db_data ) {
+				do_action( 'PLUGIN_PREFIX_on_activate' );
+			} else {
+				do_action( 'PLUGIN_PREFIX_on_update', $db_data['Version'], $file_data['Version'] );
+			}
+		}
+	}
+
+	public function update_plugin_data() {
+
+		$db_data   = get_option( PLUGIN_PREFIX_get_instance()->option_key );
+		$file_data = get_plugin_data( PLUGIN_PREFIX_get_instance()->file );
+
+		if ( ! $db_data || version_compare( $file_data['Version'], $db_data['Version'], '>' ) ) {
+
+			PLUGIN_PREFIX_get_instance()->name    = $file_data['Name'];
+			PLUGIN_PREFIX_get_instance()->version = $file_data['Version'];
+
+			update_option( PLUGIN_PREFIX_get_instance()->option_key, $file_data );
+
 			if ( ! $db_data ) {
 				do_action( 'PLUGIN_PREFIX_on_activate' );
 			} else {
@@ -105,6 +125,6 @@ class Plugin {
 	}
 
 	public function deactivate() {
-		delete_option( self::$option_key );
+		delete_option( PLUGIN_PREFIX_get_instance()->option_key );
 	}
 }
--- plugin-boilerplate-wordpress.php
+++ plugin-boilerplate-wordpress.php
@@ -10,26 +10,24 @@ Text Domain: TEXT_DOMAIN
 Domain Path: /languages
  */
 
-if ( version_compare( $wp_version, '4.7', '<' ) || version_compare( PHP_VERSION, '5.3', '<' ) ) {
-	function PLUGIN_PREFIX_compatability_warning(){
-		echo '<div class="error"><p>' . sprintf(
-			// translators: Dependency waring
-			__( '“%1$s” requires PHP %2$s (or newer) and WordPress %3$s (or newer) to function properly. Your site is using PHP %4$s and WordPress %5$s. Please upgrade. The plugin has been automatically deactivated.', 'TEXT_DOMAIN' ),
-			'PLUGIN NAME',
-			'5.3',
-			'4.7',
-			PHP_VERSION,
-			$GLOBALS['wp_version']
-		) . '</p></div>';
+if ( version_compare( $GLOBALS['wp_version'], '4.7', '<' ) || version_compare( PHP_VERSION, '5.3', '<' ) ) {
+	function PLUGIN_PREFIX_compatability_warning() {
+
+		// translators: Dependency waring
+		$dependency_text = sprintf( __( '“%1$s” requires PHP %2$s (or newer) and WordPress %3$s (or newer) to function properly. Your site is using PHP %4$s and WordPress %5$s. Please upgrade. The plugin has been automatically deactivated.', 'TEXT_DOMAIN' ), 'PLUGIN NAME', '5.3', '4.7', PHP_VERSION, $GLOBALS['wp_version'] );
+		echo "<div class='error'><p>$dependency_text</p></div>";
+
 		if ( isset( $_GET['activate'] ) ) {
 			unset( $_GET['activate'] );
 		}
 	}
+
 	add_action( 'admin_notices', 'PLUGIN_PREFIX_compatability_warning' );
 
 	function PLUGIN_PREFIX_deactivate_self() {
 		deactivate_plugins( plugin_basename( __FILE__ ) );
 	}
+
 	add_action( 'admin_init', 'PLUGIN_PREFIX_deactivate_self' );
 
 	return;
@@ -40,6 +38,7 @@ if ( version_compare( $wp_version, '4.7', '<' ) || version_compare( PHP_VERSION,
 	function PLUGIN_PREFIX_get_instance() {
 		return AUTHOR_NAMESPACE\PLUGIN_NAMESPACE\Plugin::get_instance( __FILE__ );
 	}
+
 	PLUGIN_PREFIX_get_instance();
 
 
@@ -49,22 +48,24 @@ if ( version_compare( $wp_version, '4.7', '<' ) || version_compare( PHP_VERSION,
 
 	function PLUGIN_PREFIX_update_test( $old_version = 'none', $new_version = 'none' ) {
 
-		add_action( 'admin_notices', function() use ( $old_version, $new_version ) {
+		add_action( 'admin_notices', function () use ( $old_version, $new_version ) {
 
 			echo '<div class="notice notice-success is-dismissible">';
-			echo 	'<p>PLUGIN_PREFIX_on_update: ' . $old_version . ' --> ' . $new_version . '</p>';
+			echo '<p>PLUGIN_PREFIX_on_update: ' . $old_version . ' --> ' . $new_version . '</p>';
 			echo '</div>';
-		});
+		} );
 	}
+
 	add_action( 'PLUGIN_PREFIX_on_update', 'PLUGIN_PREFIX_update_test', 10, 2 );
 
 	function PLUGIN_PREFIX_activate_test() {
 
-		add_action( 'admin_notices', function() {
+		add_action( 'admin_notices', function () {
 			echo '<div class="notice notice-success is-dismissible">';
-			echo 	'<p>PLUGIN_PREFIX_on_activate</p>';
+			echo '<p>PLUGIN_PREFIX_on_activate</p>';
 			echo '</div>';
-		});
+		} );
 	}
+
 	add_action( 'PLUGIN_PREFIX_on_activate', 'PLUGIN_PREFIX_activate_test' );
 }
